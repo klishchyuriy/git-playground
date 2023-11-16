e8387f263581828cfdc3d4c3b028df12b3602b6e
Yuriy Klishch <programmingklishch@gmail.com>
2023-11-16 22:37:05 +0200

    the user can't enter the same word twice or more

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
 
-print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
+print(f"Can you make up {
+      WORDS_TO_WIN} words from letters in word provided by me?")
 print(f"Your word is '{word}'")
 
 
@@ -47,6 +48,9 @@ while not is_game_over():
         continue
 
     if guess in full_list:
+        if guess in guesses:
+            print('You have already guessed it!')
+            continue
         guessed += 1
         guesses.append(guess)
         if guessed == WORDS_TO_WIN:
@@ -55,4 +59,4 @@ while not is_game_over():
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
     else:
         errors += 1
-        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+        print(f"Oops 🙁 No such word, you have {ERRORS_TO_LOSE - errors} lives more")
